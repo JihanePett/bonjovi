5c0025702c2a39dd9eae68b3d06762c4dff961ca
JihanePett <jihane.petticrew@gmail.com>
2020-11-05 16:51:00 +0000

    updated products_jbjxp url and template to show 2 cards

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product_detail'),
-    path('products_jbjxp/', views.products_jbjxp, name='products_jbjxp'),
+    path('', views.products_jbjxp, name='products_jbjxp'),
 ]

923d1d64d34fbc474702237c39afe4dcbf686512
JihanePett <jihane.petticrew@gmail.com>
2020-11-23 14:21:35 +0000

    added stripe elemnts js code to process payment

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,6 +1,6 @@
 /* data from https://stripe.com/docs/payments/accept-a-payment */
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 var style = {

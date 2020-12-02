4b63ed1a9be83d7a276c1c9892fb38029b3408f1
JihanePett <jihane.petticrew@gmail.com>
2020-12-02 13:41:34 +0000

    added profile url and view and updated its path in the main urls, created a profiles static folder with empty css file

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def profile(request):
+    """ To display the user profile """
+    template = 'profiles/profiles.html'
+    context = {}
+    return render(request, template, context)

27aad1a90e15d201ff02432dad70ae11e9f7102e
JihanePett <jihane.petticrew@gmail.com>
2020-12-07 14:24:11 +0000

    added send mail to customer functionality in the webhook handler and a default email address in the settings

--- bonjovi/settings.py
+++ bonjovi/settings.py
@@ -172,3 +172,5 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 # Stripe webhooks
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+# Default email
+DEFAULT_FROM_EMAIL = 'info@example.com'

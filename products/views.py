c1fc18403227ca04d60e2e7600195537e7ce8eba
JihanePett <jihane.petticrew@gmail.com>
2020-12-13 14:57:39 +0000

    created custom widget templates to manage images input, updated the add aand edit templates and views accordingly

--- products/views.py
+++ products/views.py
@@ -75,7 +75,7 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
@@ -133,4 +133,3 @@ def delete_product(request, product_id):
     product.delete()
     messages.success(request, 'Product deleted!')
     return redirect(reverse('products'))
-
